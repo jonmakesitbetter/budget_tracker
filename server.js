4fa41059b6ed88cb0e0ab09dd3789248b5204982
Jon Peach <jonmakesitbetter@gmail.com>
2020-10-27 19:22:58 -0400

    .

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/budget-tracker-0124",
+  process.env.MONGODB_URI || "mongodb://localhost/budget-tracker-0125",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
