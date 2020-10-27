2ac6ddf3fdc44496f32c05a7632bc014e9d64be3
Jon Peach <jonmakesitbetter@gmail.com>
2020-10-27 19:20:53 -0400

    Fixed server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,9 +15,21 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budget-tracker-0124",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
+const connection = mongoose.connection;
+connection.on("connected", () => {
+  console.log("Mongoose successfully connected.");
+});
+connection.on("error", (err) => {
+  console.log("Mongoose connection error: ", err);
 });
 
 // routes
